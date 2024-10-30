b0856cb644ca199a47d618500e1952f7626d31dd
Jokkere <39446379+Jokkere@users.noreply.github.com>
2024-10-30 22:17:52 +0100

    Update multisend.sh

--- multisend.sh
+++ multisend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Loop through numbers 1 to 5
-for i in {16..50}; do
+for i in {51..150}; do
   # Define the path to the configuration directory for the current number
   CONFIG_PATH="$HOME/ceremonyclient/node/$i"
 
