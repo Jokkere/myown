69bfb778b4c2fbca81c1c606a80b3d1d4ca076a2
Jokkere <39446379+Jokkere@users.noreply.github.com>
2024-10-30 22:17:03 +0100

    Update mergefive.sh

--- mergefive.sh
+++ mergefive.sh
@@ -8,10 +8,10 @@ echo "Output from token coins command:"
 echo "$output"
 
 # Extract the first 5 coin addresses from the output
-coin_addresses=($(echo "$output" | grep -oP '(?<=Coin )[a-fA-F0-9x]+' | head -n 5))
+coin_addresses=($(echo "$output" | grep -oP '(?<=Coin )[a-fA-F0-9x]+' | head -n 15))
 
 # Check if we have at least 5 coin addresses
-if [[ ${#coin_addresses[@]} -eq 5 ]]; then
+if [[ ${#coin_addresses[@]} -eq 15 ]]; then
   # Construct the merge command with the first 5 coin addresses
   merge_command="./qclient-2.0.2.3-linux-amd64 token merge ${coin_addresses[0]} ${coin_addresses[1]} ${coin_addresses[2]} ${coin_addresses[3]} ${coin_addresses[4]} --config $HOME/ceremonyclient/node/.config"
   
@@ -19,5 +19,5 @@ if [[ ${#coin_addresses[@]} -eq 5 ]]; then
   echo "Executing: $merge_command"
   eval $merge_command
 else
-  echo "Less than 5 coin addresses found. Merge command not executed."
+  echo "Less than 15 coin addresses found. Merge command not executed."
 fi
