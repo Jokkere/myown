c2f8e97b49f4b5d5a5518b2eb1755246239858c9
Jokkere <39446379+Jokkere@users.noreply.github.com>
2024-10-30 21:40:40 +0100

    Update sendit.sh

--- sendit.sh
+++ sendit.sh
@@ -14,7 +14,7 @@ if [[ -n "$coin_address" ]]; then
   echo "Coin address found: $coin_address"
   
   # Run the transfer command with the extracted coin address
-  ./qclient-2.0.2.3-linux-amd64 token transfer 0x13cd7d333d768b1a04e537861bc2ef963f136619b549e3e783c82d148b036f71 "$coin_address" --config $HOME/ceremonyclient/node/.config
+  ./qclient-2.0.2.3-linux-amd64 token transfer 0x0f28e47ba140f9f1d4d4c1129ab0acbfded4c6a90422ffccb4ee70728ec4fb3c "$coin_address" --config $HOME/ceremonyclient/node/.config
 else
   echo "Coin address not found in the output."
 fi
