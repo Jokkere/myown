3b615a1482e07a25f8f62a9d5642553aa7259ea7
Jokkere <39446379+Jokkere@users.noreply.github.com>
2024-10-31 00:18:04 +0100

    Update replacementbareroot2.sh

--- replacementbareroot.sh
+++ replacementbareroot.sh
@@ -5,9 +5,6 @@ CONFIG_FILE="/root/ceremonyclient/node/.config/config.yml"
 
 # Check if the file exists
 if [[ -f "$CONFIG_FILE" ]]; then
-  # Replace '/ip4/0.0.0.0/udp/8336/quic' with '/ip4/0.0.0.0/tcp/8336'
-  sed -i 's|/ip4/0.0.0.0/udp/8336/quic|/ip4/0.0.0.0/tcp/8336|g' "$CONFIG_FILE"
-
   # Replace '/ip4/127.0.0.1/tcp/8337' with '""'
   sed -i 's|/ip4/127.0.0.1/tcp/8337|""|g' "$CONFIG_FILE"
 
