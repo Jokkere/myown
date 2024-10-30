fd1198f35e74048dadff0cbdf5193fb4301cd2cd
Jokkere <39446379+Jokkere@users.noreply.github.com>
2024-10-30 23:38:49 +0100

    Update and rename replacementip.sh to replacementbare.sh

--- replacementbare.sh
+++ replacementbare.sh
@@ -11,6 +11,9 @@ if [[ -f "$CONFIG_FILE" ]]; then
   # Replace '/ip4/127.0.0.1/tcp/8337' with '""'
   sed -i 's|/ip4/127.0.0.1/tcp/8337|""|g' "$CONFIG_FILE"
 
+  # Replace '.config/keys.yml' with '/root/ceremonyclient/node/.config/keys.yml'
+  sed -i 's|.config/keys.yml|/root/ceremonyclient/node/.config/keys.yml|g' "$CONFIG_FILE"
+
   echo "Replacement complete."
 else
   echo "Configuration file not found at $CONFIG_FILE"
