5febad6d4b52493605952ec48005b0a3c093ffac
Jokkere <39446379+Jokkere@users.noreply.github.com>
2024-10-31 00:20:59 +0100

    Update and rename replacementbareroot2.sh to replacementbareroot3.sh

--- replacementbareroot3.sh
+++ replacementbareroot3.sh
@@ -6,7 +6,7 @@ CONFIG_FILE="/root/ceremonyclient/node/.config/config.yml"
 # Check if the file exists
 if [[ -f "$CONFIG_FILE" ]]; then
   # Replace '/ip4/127.0.0.1/tcp/8337' with '""'
-  sed -i 's|/ip4/127.0.0.1/tcp/8337|""|g' "$CONFIG_FILE"
+  sed -i 's|"/ip4/127.0.0.1/tcp/8337"|""|g' "$CONFIG_FILE"
 
   # Replace '.config/keys.yml' with '/root/ceremonyclient/node/.config/keys.yml'
   sed -i 's|.config/store|/root/ceremonyclient/node/.config/store|g' "$CONFIG_FILE"
