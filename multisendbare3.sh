c1746ceb0812b7f2d52bbbb99790099f21232588
Jokkere <39446379+Jokkere@users.noreply.github.com>
2024-10-31 17:14:23 +0100

    Update and rename multisendbare2.sh to multisendbare3.sh

--- multisendbare3.sh
+++ multisendbare3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Loop through numbers 1 to 5
-for i in {45..402}; do
+for i in {110..200}; do
   # Define the path to the configuration directory for the current number
   CONFIG_PATH="$HOME/ceremonyclient/node/$i/.config"
 
