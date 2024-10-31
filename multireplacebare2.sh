00afdf5e456a60aecacc985042ea4df084150596
Jokkere <39446379+Jokkere@users.noreply.github.com>
2024-10-31 03:12:37 +0100

    Update and rename multireplacebare.sh to multireplacebare2.sh

--- multireplacebare2.sh
+++ multireplacebare2.sh
@@ -1,7 +1,7 @@
 # Loop through numbers 1 to 200
 for i in {1..200}; do
   # Define the path to the YAML file for the current number
-  CONFIG_FILE="/root/ceremonyclient/node/$i/config.yml"
+  CONFIG_FILE="/root/ceremonyclient/node/$i/.config/config.yml"
 
   # Check if the file exists
   if [[ -f "$CONFIG_FILE" ]]; then
@@ -9,7 +9,7 @@ for i in {1..200}; do
     sed -i 's|/ip4/127.0.0.1/tcp/8337|""|g' "$CONFIG_FILE"
 
     # Replace '.config/store' with '/root/ceremonyclient/node/<number>/store', where <number> is the current value of i
-    sed -i "s|.config/store|/root/ceremonyclient/node/$i/store|g" "$CONFIG_FILE"
+    sed -i "s|.config/store|/root/ceremonyclient/node/$i/.config/store|g" "$CONFIG_FILE"
 
     echo "Replacement complete in $CONFIG_FILE."
   else
