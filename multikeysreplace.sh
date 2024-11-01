8d5eaf87fc23b29b8cf2d7f47080c43d73c670a2
Jokkere <39446379+Jokkere@users.noreply.github.com>
2024-11-01 01:45:30 +0100

    Create multikeysreplace.sh

---
+++ multikeysreplace.sh
@@ -0,0 +1,21 @@
+# Loop through numbers 1 to 200
+for i in {1..407}; do
+  # Define the path to the YAML file for the current number
+  CONFIG_FILE="/root/ceremonyclient/node/$i/config.yml"
+
+  # Check if the file exists
+  if [[ -f "$CONFIG_FILE" ]]; then
+    # Replace '/ip4/127.0.0.1/tcp/8337' with '""'
+    sed -i 's|"/ip4/127.0.0.1/tcp/8337"|""|g' "$CONFIG_FILE"  
+    # Replace '/ip4/127.0.0.1/tcp/8337' with '""'
+    sed -i 's|/ip4/127.0.0.1/tcp/8337|""|g' "$CONFIG_FILE"
+    # Replace '.config/keys.yml' with '/root/ceremonyclient/node/.config/keys.yml'
+    sed -i "s|path: .config/store|path: /root/ceremonyclient/node/$i/.config/store|g" "$CONFIG_FILE"
+    # Replace '/ip4/0.0.0.0/udp/8336/quic' with '/ip4/0.0.0.0/tcp/8336'
+    sed -i 's|/ip4/0.0.0.0/udp/8336/quic|/ip4/0.0.0.0/tcp/8336|g' "$CONFIG_FILE"
+
+    echo "Replacement complete in $CONFIG_FILE."
+  else
+    echo "Configuration file not found at $CONFIG_FILE"
+  fi
+done
