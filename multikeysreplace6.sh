dcb2ddcf76caf651003de04f2b8964524ba14ee1
Jokkere <39446379+Jokkere@users.noreply.github.com>
2024-11-14 19:19:31 +0100

    Update and rename multikeysreplace5.sh to multikeysreplace6.sh

--- multikeysreplace6.sh
+++ multikeysreplace6.sh
@@ -7,11 +7,11 @@ for i in {1..1}; do
   # Check if the file exists
   if [[ -f "$CONFIG_FILE" ]]; then
     # Replace '/ip4/127.0.0.1/tcp/8337' with '""'
-    sed -i 's|"/ip4/127.0.0.1/tcp/8337"|""|g' "$CONFIG_FILE"  
+    sed -i 's|"/ip4/0.0.0.0/tcp/8337"|""|g' "$CONFIG_FILE"  
     # Replace '/ip4/127.0.0.1/tcp/8337' with '""'
-    sed -i 's|/ip4/127.0.0.1/tcp/8337|""|g' "$CONFIG_FILE"
+    sed -i 's|/ip4/0.0.0.0/tcp/8337|""|g' "$CONFIG_FILE"
     # Replace '/ip4/127.0.0.1/tcp/8338' with '""'
-    sed -i 's|/ip4/127.0.0.1/tcp/8338|""|g' "$CONFIG_FILE"
+    sed -i 's|/ip4/0.0.0.0/tcp/8338|""|g' "$CONFIG_FILE"
     # Replace '/ip4/0.0.0.0/udp/8336/quic' with '/ip4/0.0.0.0/tcp/8336'
     sed -i 's|/ip4/0.0.0.0/udp/8336/quic|/ip4/0.0.0.0/tcp/8336|g' "$CONFIG_FILE"
 
